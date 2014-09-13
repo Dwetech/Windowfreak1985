2b991f36977218e6f93db73e6e70fcb237825e95
mohammad faozul azim rafi <rafi@dwetech.com>
2014-09-13 16:58:29 +0600

    file name ext is php

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Life Department - Login</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+    <!--font css-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    
+	<!-- style sheet css -->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+	<!--header div-->
+	<div class="header-bg blue">
+    	<div class="container">
+        	<div class="row">
+            	<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 centeralign">
+                	<a href="#">
+                    	<img src="img/logo.png" alt="Life Department" />
+                    </a>
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                	<ul class="headlogin text-center headlogin1">
+                    	<li><input type="text" value="" placeholder="email" class="emailtext" />
+						<p class="keep"><input type="checkbox" name="" class="cbox"/>Keep me logged in</p>
+						</li>
+                        <li><input type="password" value="" placeholder="password" class="passtext" />
+						<p class="keep">forgot your password?</p>
+						</li>
+                        <li><input type="button" value="Login" name="Login" class="loginbtn" /></li>
+                    </ul>
+                    <div class="c"></div>
+                    <ul class="keep-forgot">
+                    	<li></li>
+                        
+                    </ul>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--header div end-->
+    
+    <div class="container" style="margin-top:30px;">
+        	<div class="row banner" >
+            	<img src="img/bg-babber'.jpg"  class="img-responsive" />
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center visittext">
+            	<h3>want to learn more? visit www.yourlifedepartment.com<br/>
+or call 800-990-9993</h3>
+				<p>A SERVICE OF Designs In Life Insurance Marketing, LLC</p>
+            </div>
+       
+    </div>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Life Department - View</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+    <!--font css-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    
+	<!-- style sheet css -->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+	<!--header div-->
+	<div class="header-bg blue">
+    	<div class="container">
+        	<div class="row">
+            	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 centeralign">
+                	<a href="#">
+                    	<img src="img/logo.png" alt="Life Department" />
+                    </a>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                	<ul class="headlogin text-center">
+                    	<li><a href="#">ADMINISTRATOR</a></li>
+                        <li>|</li>
+                        <li><a href="#">Sign-out <i class="fa fa-sign-out"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--header div end-->
+    
+    <!--table lay out div-->
+    <div class="container">
+    	<div class="topmargin">
+        	<div class="summary">
+                <img src="img/refeces.jpg" alt="r1" class="img-responsive pull-left" style="border-radius:5px 0px 0px 5px; border-right:1px solid #d2d2d2;"/>
+                <p class="tabletext pull-left" style="padding:10px 20px; width:81%;text-align:justify">I noticed that we don’t have your life insurance policy on file. There have been alot of changes in the industry and we may be able to save you some money on your Life Insurnace, can I schedule a call with our life department for a no-obligation review of your coverage?</p>
+				<div class="c"></div>
+            </div>
+        </div>
+    </div>
+        
+        <div class="container" style="margin-top:20px;">
+        <div class="row">
+        	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            	<h2 class="" style="font-size:26px; line-height:30px;">New Leads</h2>
+				<div class="clearfix"></div>
+                <div id="reg">
+                	<input class="form-control1 inputone1" type="text" placeholder="FIRSTNAME">
+                    <input class="form-control1" type="text" placeholder="LASTNAME">
+                    <select class="form-control1">
+                        <option>LEAD RESULT</option>
+                        <option>LEAD RESULT</option>
+                        <option>LEAD RESULT</option>
+                        <option>LEAD RESULT</option>
+                    </select>
+                    <select class="form-control1">
+                        <option>TIME TO CALL</option>
+                        <option>LEAD RESULT</option>
+                        <option>LEAD RESULT</option>
+                        <option>LEAD RESULT</option>
+                    </select>
+                    <input class="form-control1" type="text" placeholder="PHONE NUMBER">
+                    <input class="form-control1" type="text" placeholder="NOTES">
+                    <input type="submit" value="SUBMIT" class="form-control1 btn1">
+					<div class="clearfix"></div>
+                </div>
+			</div>
+        </div>
+        </div>
+        
+        <div class="container">
+        <div class="row" style="margin-top:20px;">
+        	<div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+            	<h2 class="admin-box" style="margin:0px; line-height:30px;">All Leads</h2>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>first name</th>
+                            <th>last name</th>
+                            <th>phone number</th>
+                            <th>time to call</th>
+                            <th>yes/no</th>
+                            <th>date/time</th>
+                            
+                        </tr>
+                        <tr>
+                            <td>Michael</td>
+                            <td>Smith</td>
+                            <td>555-123-4567</td>
+                            <td>8:00am</td>
+                            <td>YES - call them</td>
+                            <td style="text-align:left;">1/1/14 - 10:49am</td>
+                            
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </table>
+                </div>
+                <ul class="pagebtn">
+                    <li><a href="#" class="activ">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">Next</a></li>
+                    <li><a href="#">Last</a></li>
+                </ul>
+            </div>
+            
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                <div id="summ" class="summary">
+                    <h3>Summary</h3>
+                        <hr style="height:1px; border-top:1px solid #d2d2d2; width:100%; margin:0px;"/>
+                        <table class="table noborder">
+                        <tr>
+                            <td><span class="leadtitle">Leads Today</span> <span>yes: 2</span> <span>No:10</span></td>
+                        </tr>
+                        <tr>
+                            <td><span class="leadtitle">Leads This Week</span> <span>yes: 2</span> <span>No:10</span></td>
+                        </tr>
+                        <tr>
+                            <td><span class="leadtitle">Leads This Month</span> <span>yes: 2</span> <span>No:10</span></td>
+                        </tr>
+                        
+                    </table>
+                </div>
+                <div class="summary" style="margin-top:20px;">
+                    <h3>Talk to a Specialist</h3>
+                        <hr style="height:1px; border-top:1px solid #d2d2d2; width:100%; margin:0px;"/>
+                      <p style="margin:10px;font-size:16px; color:#626262;"><i class="fa fa-phone"></i> 123 1234 1234</p>
+                </div>
+                <div class="c"></div>
+            </div>
+            
+        </div>
+    </div>
+ </div>
+    <!--table lay out div end-->
+</body>
+</html>
