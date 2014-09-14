c1d7964af62ca932cbea777e3191255419bd2300
Abdullah Al Jahid <abdullah.al.jahid@gmail.com>
2014-09-14 18:18:18 +0600

    banner header fix

--- admin/banner/add-banner.php
+++ admin/banner/add-banner.php
@@ -7,9 +7,16 @@ include '../../core.php';
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Life Department - View</title>
-        <?php
-        include ROOT_DIR . 'include/head.php';
-        ?>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="<?php echo CSS; ?>/bootstrap.css" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+
+        <!--font css-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'/>
+
+        <!-- style sheet css -->
+        <link href="<?php echo CSS; ?>/font-awesome.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="<?php echo WEBSITE_URL ?>style.css" type="text/css"/>
     </head>
 
     <body>
