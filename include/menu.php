b2cd4f311e4ecaa1e142a606127542083896c83d
Ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2014-09-15 17:22:00 +0600

    random

--- include/menu.php
+++ include/menu.php
@@ -8,7 +8,7 @@
                     <li><a href="<?php echo WEBSITE_URL; ?>admin/user/user.php">users</a></li>
                     <li><a href="<?php echo WEBSITE_URL; ?>admin/starter/starters.php">starters</a></li>
                     <li><a href="<?php echo WEBSITE_URL; ?>admin/banner/banner.php">banners</a></li>
-                    <li><a class="active" href="<?php echo WEBSITE_URL; ?>admin/administrator/administrator.php">administrators</a></li>
+                    <li><a href="<?php echo WEBSITE_URL; ?>admin/administrator/administrator.php">administrators</a></li>
                 </ul>
             </div>
         </div>
